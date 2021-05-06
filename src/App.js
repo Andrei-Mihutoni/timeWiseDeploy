c8e87cabbd2fe23758db33185ebd93d663cd44ee
nitzanmoise <nitzanmois@gmail.com>
2021-05-06 15:28:33 +0300

    nitzan#

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
+import ReactDOM from "react-dom";
 
 import { NavBar, Footer, Loading } from "./components";
 import { HomePage, Profile, ExternalApi } from "./views";
+import { useAuth0 } from "@auth0/auth0-react";
 import ProtectedRoute from "./auth/protected-route";
+
+import CustomCalendar1 from "./CustomCalendar1";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Calendar from "./pages/Calendar";
+import Home from "./pages/Home";
+import Requests from "./pages/Requests";
+import { Appbar, Button, Container } from "muicss/react";
+
+import UserList from "./containers/user-list";
 
 import "./App.scss";
 
-const App = () => {
+function App() {
   const { isLoading } = useAuth0();
 
   if (isLoading) {
@@ -16,18 +25,20 @@ const App = () => {
   }
 
   return (
-    <div id="app" className="d-flex flex-column h-100">
-      <NavBar />
-      <div className="container flex-grow-1">
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <ProtectedRoute path="/profile" component={Profile} />
-          <ProtectedRoute path="/external-api" component={ExternalApi} />
-        </Switch>
-      </div>
-      <Footer />
+    <div>
+      <Router>
+        <NavBar />
+        <div className="container flex-grow-1">
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <ProtectedRoute path="/profile" component={Profile} />
+            <ProtectedRoute path="/external-api" component={ExternalApi} />
+          </Switch>
+        </div>
+        <Footer />
+      </Router>
     </div>
   );
-};
+}
 
 export default App;
