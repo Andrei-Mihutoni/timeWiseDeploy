590fd71f3e703b7338a2367fd37d1709acda840d
Andrei Mihutoni <andrei.mihutoni@gmail.com>
2021-05-10 21:08:16 +0200

    Creating the backend and connecting the external API backend with the public/protected message buttons

---
+++ backend/src/messages/messages.router.js
@@ -0,0 +1,43 @@
+/**
+ * Required External Modules and Interfaces
+ */
+
+const express = require("express");
+const { getPublicMessage, getProtectedMessage } = require("./messages.service");
+const { checkJwt } = require("../authz/check-jwt");
+
+const { jwtAuthz } = require("../authz/check-jwt");
+
+
+
+/**
+ * Router Definition
+ */
+
+const messagesRouter = express.Router();
+
+/**
+ * Controller Definitions
+ */
+
+// GET messages/
+
+messagesRouter.get("/public-message", (req, res) => {
+  const message = getPublicMessage();
+  res.status(200).send(message);
+});
+
+messagesRouter.get("/protected-message", checkJwt, (req, res) => {
+  const message = getProtectedMessage();
+  res.status(200).send(message);
+});
+
+messagesRouter.get("/role", checkJwt, jwtAuthz, (req, res) => {
+  const message = getRoleBasedMessage();
+  res.status(200).send(message);
+});
+
+
+module.exports = {
+  messagesRouter,
+};
